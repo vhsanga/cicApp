1f9841490069f1f39651838bf1e39f75d9dc3c6a
Victor Sanga <vsanga@devpc.nodoclic>
2021-07-10 01:26:53 -0500

    primer commit

---
+++ app/src/main/java/com/nodoclic/cic/ui/home/HomeFragment.java
@@ -0,0 +1,276 @@
+package com.nodoclic.cic.ui.home;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.MifareUltralight;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.nodoclic.cic.R;
+import com.nodoclic.cic.databinding.FragmentHomeBinding;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private FragmentHomeBinding binding;
+    //Intialize attributes
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    private static final String TAG ="TAG_NFC";
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+                resolveIntent(getActivity().getIntent());
+            }
+        });
+
+
+        //Initialise NfcAdapter
+        nfcAdapter = NfcAdapter.getDefaultAdapter(getContext());
+        //If no NfcAdapter, display that the device has no NFC
+        if (nfcAdapter == null){
+            Toast.makeText(getContext(),"NO NFC Capabilities",  Toast.LENGTH_SHORT).show();
+            getActivity().finish();
+        }
+        //Create a PendingIntent object so the Android system can
+        //populate it with the details of the tag when it is scanned.
+        //PendingIntent.getActivity(Context,requestcode(identifier for
+        //                           intent),intent,int)
+        pendingIntent = PendingIntent.getActivity(getContext(),0,new Intent(getContext(),getContext().getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
+
+        return root;
+    }
+
+    private void resolveIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Tag tag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            assert tag != null;
+            byte[] payload = detectTagData(tag).getBytes();
+        }
+    }
+
+
+    private String detectTagData(Tag tag) {
+        StringBuilder sb = new StringBuilder();
+        byte[] id = tag.getId();
+        sb.append("ID (hex): ").append(toHex(id)).append('\n');
+        sb.append("ID (reversed hex): ").append(toReversedHex(id)).append('\n');
+        sb.append("ID (dec): ").append(toDec(id)).append('\n');
+        sb.append("ID (reversed dec): ").append(toReversedDec(id)).append('\n');
+
+        String prefix = "android.nfc.tech.";
+        sb.append("Technologies: ");
+        for (String tech : tag.getTechList()) {
+            sb.append(tech.substring(prefix.length()));
+            sb.append(", ");
+        }
+
+        sb.delete(sb.length() - 2, sb.length());
+
+        for (String tech : tag.getTechList()) {
+            if (tech.equals(MifareClassic.class.getName())) {
+                sb.append('\n');
+                String type = "Unknown";
+
+                try {
+                    MifareClassic mifareTag = MifareClassic.get(tag);
+
+                    switch (mifareTag.getType()) {
+                        case MifareClassic.TYPE_CLASSIC:
+                            type = "Classic";
+                            break;
+                        case MifareClassic.TYPE_PLUS:
+                            type = "Plus";
+                            break;
+                        case MifareClassic.TYPE_PRO:
+                            type = "Pro";
+                            break;
+                    }
+                    sb.append("Mifare Classic type: ");
+                    sb.append(type);
+                    sb.append('\n');
+
+                    sb.append("Mifare size: ");
+                    sb.append(mifareTag.getSize() + " bytes");
+                    sb.append('\n');
+
+                    sb.append("Mifare sectors: ");
+                    sb.append(mifareTag.getSectorCount());
+                    sb.append('\n');
+
+                    sb.append("Mifare blocks: ");
+                    sb.append(mifareTag.getBlockCount());
+                } catch (Exception e) {
+                    sb.append("Mifare classic error: " + e.getMessage());
+                }
+            }
+
+            if (tech.equals(MifareUltralight.class.getName())) {
+                sb.append('\n');
+                MifareUltralight mifareUlTag = MifareUltralight.get(tag);
+                String type = "Unknown";
+                switch (mifareUlTag.getType()) {
+                    case MifareUltralight.TYPE_ULTRALIGHT:
+                        type = "Ultralight";
+                        break;
+                    case MifareUltralight.TYPE_ULTRALIGHT_C:
+                        type = "Ultralight C";
+                        break;
+                }
+                sb.append("Mifare Ultralight type: ");
+                sb.append(type);
+            }
+        }
+        Log.v("test",sb.toString());
+        return sb.toString();
+    }
+    private String toHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = bytes.length - 1; i >= 0; --i) {
+            int b = bytes[i] & 0xff;
+            if (b < 0x10)
+                sb.append('0');
+            sb.append(Integer.toHexString(b));
+            if (i > 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+
+    private String toReversedHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; ++i) {
+            if (i > 0) {
+                sb.append(" ");
+            }
+            int b = bytes[i] & 0xff;
+            if (b < 0x10)
+                sb.append('0');
+            sb.append(Integer.toHexString(b));
+        }
+        return sb.toString();
+    }
+
+    private long toDec(byte[] bytes) {
+        long result = 0;
+        long factor = 1;
+        for (int i = 0; i < bytes.length; ++i) {
+            long value = bytes[i] & 0xffl;
+            result += value * factor;
+            factor *= 256l;
+        }
+        return result;
+    }
+
+    private long toReversedDec(byte[] bytes) {
+        long result = 0;
+        long factor = 1;
+        for (int i = bytes.length - 1; i >= 0; --i) {
+            long value = bytes[i] & 0xffl;
+            result += value * factor;
+            factor *= 256l;
+        }
+        return result;
+    }
+
+
+    public void writeTag(MifareUltralight mifareUlTag) {
+        try {
+            mifareUlTag.connect();
+            mifareUlTag.writePage(4, "get ".getBytes(Charset.forName("US-ASCII")));
+            mifareUlTag.writePage(5, "fast".getBytes(Charset.forName("US-ASCII")));
+            mifareUlTag.writePage(6, " NFC".getBytes(Charset.forName("US-ASCII")));
+            mifareUlTag.writePage(7, " now".getBytes(Charset.forName("US-ASCII")));
+        } catch (IOException e) {
+            Log.e(TAG, "IOException while writing MifareUltralight...", e);
+        } finally {
+            try {
+                mifareUlTag.close();
+            } catch (IOException e) {
+                Log.e(TAG, "IOException while closing MifareUltralight...", e);
+            }
+        }
+    }
+    public String readTag(MifareUltralight mifareUlTag) {
+        try {
+            mifareUlTag.connect();
+            byte[] payload = mifareUlTag.readPages(4);
+            return new String(payload, Charset.forName("US-ASCII"));
+        } catch (IOException e) {
+            Log.e(TAG, "IOException while reading MifareUltralight message...", e);
+        } finally {
+            if (mifareUlTag != null) {
+                try {
+                    mifareUlTag.close();
+                }
+                catch (IOException e) {
+                    Log.e(TAG, "Error closing tag...", e);
+                }
+            }
+        }
+        return null;
+    }
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        assert nfcAdapter != null;
+        //nfcAdapter.enableForegroundDispatch(context,pendingIntent,
+        //                                    intentFilterArray,
+        //                                    techListsArray)
+        nfcAdapter.enableForegroundDispatch(getActivity(),pendingIntent,null,null);
+    }
+
+
+    public void onPause() {
+        super.onPause();
+        //Onpause stop listening
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(getActivity());
+        }
+    }
+
+
+
+
+}
