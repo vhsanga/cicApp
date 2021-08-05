cfee34a06c3b19299d5ed57fcbdb883d84bdbf47
Victor Sanga <vsanga@debiandev.nodoclic>
2021-08-05 00:54:21 -0500

    avances de menu

--- app/src/main/java/com/nodoclic/cic/ui/home/HomeFragment.java
+++ app/src/main/java/com/nodoclic/cic/ui/home/HomeFragment.java
@@ -38,6 +38,7 @@ import com.nodoclic.cic.util.CONST;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
@@ -49,9 +50,18 @@ public class HomeFragment extends Fragment {
     PendingIntent pendingIntent;
     private static final String TAG ="TAG_NFC";
     private Context context;
-    protected MenuAdapter mAdapter;
+    protected MenuAdapter mAdapter_1;
     protected RecyclerView mRecyclerView_1;
-    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView.LayoutManager mLayoutManager_1;
+    protected TextView tvDesayuno;
+    protected MenuAdapter mAdapter_2;
+    protected RecyclerView mRecyclerView_2;
+    RecyclerView.LayoutManager mLayoutManager_2;
+    protected TextView tvAlmuerzo;
+    protected MenuAdapter mAdapter_3;
+    protected RecyclerView mRecyclerView_3;
+    RecyclerView.LayoutManager mLayoutManager_3;
+    protected TextView tvMerienda;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -61,15 +71,14 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         context = root.getContext();
-        mRecyclerView_1 = (RecyclerView) root.findViewById(R.id.rv_menu_1);
-        mRecyclerView_1.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(getActivity());
-        mRecyclerView_1.setLayoutManager(mLayoutManager);
-        mRecyclerView_1.setItemAnimator(new DefaultItemAnimator());
-
-
+        initLista_1(root);
+        initLista_2(root);
+        initLista_3(root);
 
         final TextView textView = binding.textHome;
+        tvDesayuno = (TextView) root.findViewById(R.id.tvDesayuno);
+        tvAlmuerzo = (TextView) root.findViewById(R.id.tvAlmuerzo);
+        tvMerienda = (TextView) root.findViewById(R.id.tvMerienda);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -83,8 +92,38 @@ public class HomeFragment extends Fragment {
                 @Override
                 public void run() {
                     List<RsMenu> menu = Servicio._menuDia(context);
-                    mAdapter = new MenuAdapter(menu);
-                    mRecyclerView_1.setAdapter(mAdapter);
+                    List<RsMenu> menuDesayuno= new ArrayList<>();
+                    List<RsMenu> menuAlmuerzo= new ArrayList<>();
+                    List<RsMenu> menuMerienda= new ArrayList<>();
+                    for (RsMenu item: menu){
+                        if(item.getComida() == 1){
+                            menuDesayuno.add(item);
+                        }
+                        if(item.getComida() == 2){
+                            menuAlmuerzo.add(item);
+                        }
+                        if(item.getComida() == 3){
+                            menuMerienda.add(item);
+                        }
+                    }
+                    if(menuDesayuno.isEmpty()){
+                        tvDesayuno.setVisibility(View.INVISIBLE);
+                    }
+                    if(menuAlmuerzo.isEmpty()){
+                        tvAlmuerzo.setVisibility(View.INVISIBLE);
+                    }
+                    if(menuMerienda.isEmpty()){
+                        tvMerienda.setVisibility(View.INVISIBLE);
+                    }
+
+                    mAdapter_1 = new MenuAdapter(menuDesayuno);
+                    mRecyclerView_1.setAdapter(mAdapter_1);
+                    mAdapter_2 = new MenuAdapter(menuAlmuerzo);
+                    mRecyclerView_2.setAdapter(mAdapter_2);
+                    mAdapter_3 = new MenuAdapter(menuMerienda);
+                    mRecyclerView_3.setAdapter(mAdapter_3);
+
+
 
 
                 }
@@ -95,6 +134,28 @@ public class HomeFragment extends Fragment {
         }
 
         return root;
+    }
+
+    private void initLista_1(View root ){
+        mRecyclerView_1 = (RecyclerView) root.findViewById(R.id.rv_menu_1);
+        mRecyclerView_1.setHasFixedSize(true);
+        mLayoutManager_1 = new LinearLayoutManager(getActivity());
+        mRecyclerView_1.setLayoutManager(mLayoutManager_1);
+        mRecyclerView_1.setItemAnimator(new DefaultItemAnimator());
+    }
+    private void initLista_2(View root ){
+        mRecyclerView_2 = (RecyclerView) root.findViewById(R.id.rv_menu_2);
+        mRecyclerView_1.setHasFixedSize(true);
+        mLayoutManager_2 = new LinearLayoutManager(getActivity());
+        mRecyclerView_2.setLayoutManager(mLayoutManager_2);
+        mRecyclerView_2.setItemAnimator(new DefaultItemAnimator());
+    }
+    private void initLista_3(View root ){
+        mRecyclerView_3 = (RecyclerView) root.findViewById(R.id.rv_menu_3);
+        mRecyclerView_3.setHasFixedSize(true);
+        mLayoutManager_3 = new LinearLayoutManager(getActivity());
+        mRecyclerView_3.setLayoutManager(mLayoutManager_3);
+        mRecyclerView_3.setItemAnimator(new DefaultItemAnimator());
     }
 
 
